4580e38c6444242a1954dd05a95712e6ffcf006a
adeeb <adeebonsteam@gmail.com>
2020-03-18 11:04:27 +0200

    MyAssignments

---
+++ Advanced Programming/StringProject.java
@@ -0,0 +1,29 @@
+package Task2;
+
+public class StringProject {
+	public static void main(String[] args) {
+		
+		String str = "ADEEB"; //String 1
+		String str2 = "shaban"; //String 2
+		String str3 = "A";
+		
+		System.out.println("1: "+str.length()); //Length of the character
+		System.out.println("2: "+str.charAt(4)); //Specific Index 
+		System.out.println("3: "+str.equals(str2)); //To compare two strings and checks if they are equal or not (Boolean)
+		System.out.println("4: "+str.startsWith(str3)); //
+	    System.out.println("5: "+str.toLowerCase());
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+
+		
+		
+		
+		
+		
+	}
+}
